4ed3c35a0b1815228bb359fe1f70f03b97e171ff
keshav6O9 <keshav.vaishnav.12@gmail.com>
2023-10-08 23:40:20 +0530

    add azure deployment files

--- performancePulse/settings.py
+++ performancePulse/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-m(0r$br&28$ml8^@g+-i8-idyh62#n8c!%)j@z5^vixq&-^38c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
